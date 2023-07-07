b957cb9f3cec3a460d390605b88cc74070105a42
Jaredglass3 <Jaredglass352@gmail.com>
2023-07-07 10:39:41 -0500

    Task 7

--- unity-assets_models_textures/Assets/Scripts/PlayerController.cs
+++ unity-assets_models_textures/Assets/Scripts/PlayerController.cs
@@ -5,10 +5,12 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 5f;
     public float jumpForce = 5f;
     private Rigidbody playerRigidbody;
+    private Vector3 startPosition;
 
     private void Start()
     {
         playerRigidbody = GetComponent<Rigidbody>();
+        startPosition = transform.position;
     }
 
     private void Update()
@@ -19,10 +21,21 @@ public class PlayerController : MonoBehaviour
         Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical) * moveSpeed * Time.deltaTime;
         playerRigidbody.MovePosition(transform.position + movement);
 
+        if (transform.position.y < -10f)
+        {
+            ResetPlayerPosition();
+        }
+
         if (Input.GetButtonDown("Jump"))
         {
             playerRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
     }
-}
 
+    private void ResetPlayerPosition()
+    {
+        transform.position = startPosition;
+        playerRigidbody.velocity = Vector3.zero;
+        playerRigidbody.angularVelocity = Vector3.zero;
+    }
+}
