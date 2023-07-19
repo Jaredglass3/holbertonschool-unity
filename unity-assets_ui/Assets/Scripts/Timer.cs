57e293f5f4c500b4fa4e79d4627fc5712eb6f21a
Jaredglass3 <Jaredglass352@gmail.com>
2023-07-19 16:07:03 -0500

    Attempt at task 10

--- unity-assets_ui/Assets/Scripts/Timer.cs
+++ unity-assets_ui/Assets/Scripts/Timer.cs
@@ -1,4 +1,3 @@
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,6 +7,9 @@ public class Timer : MonoBehaviour
 {
     public Text TimerText;
     public float time = 0f;
+
+    public Text FinalTime;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +19,14 @@ public class Timer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Counter time, in specific format
         time += Time.deltaTime;
         TimerText.text = time.ToString("0:00.00");
+    }
+
+    public void Win()
+    {
+        // time shown on WinCanvas
+        FinalTime.text = TimerText.text;
     }
 }
