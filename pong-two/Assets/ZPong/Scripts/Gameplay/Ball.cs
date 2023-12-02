2dd45c84da454986ee69413e3e2731e4d79b0eee
Jaredglass3 <Jaredglass352@gmail.com>
2023-12-02 10:52:47 -0600

    Adding a squish animation for when my bball hits the paddles

--- pong-two/Assets/ZPong/Scripts/Gameplay/Ball.cs
+++ pong-two/Assets/ZPong/Scripts/Gameplay/Ball.cs
@@ -1,13 +1,15 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.UI;
+
 namespace ZPong
 {
-
     public class Ball : MonoBehaviour
     {
         public float speed = 5f;
+        public float squishScale = 0.5f;
+        public float squishDuration = 0.2f;
+        private bool isSquishing = false;
 
         private float screenTop = 527;
         private float screenBottom = -527;
@@ -20,7 +22,6 @@ namespace ZPong
         protected RectTransform rectTransform;
 
         private AudioSource bounceSFX;
-
 
         private void Start()
         {
@@ -40,27 +41,23 @@ namespace ZPong
             if (PlayerPrefs.HasKey("PitchDirection"))
             {
                 string pitchDirectionValue = PlayerPrefs.GetString("PitchDirection");
-    
+
                 if (pitchDirectionValue == "Random")
                 {
-                    // Generate a random direction between -1 and 1 for the x-axis.
                     float randomX = Random.Range(-1f, 1f);
                     direction = new Vector2(randomX, 0f).normalized;
                 }
                 else if (pitchDirectionValue == "Right")
                 {
-                    // Set the direction to move right.
                     direction = new Vector2(1f, 0f);
                 }
                 else
                 {
-                    // Default to moving left if the value is not recognized.
                     direction = new Vector2(-1f, 0f);
                 }
             }
             else
             {
-                // Default to moving left if no value is found in PlayerPrefs.
                 direction = new Vector2(-1f, 0f);
             }
 
@@ -75,12 +72,8 @@ namespace ZPong
         {
             if (ballActive)
             {
-
-
                 Vector2 newPosition = rectTransform.anchoredPosition + (direction * speed * Time.deltaTime);
-
                 rectTransform.anchoredPosition = newPosition;
-
 
                 if (rectTransform.anchoredPosition.y >= screenTop || rectTransform.anchoredPosition.y <= screenBottom)
                 {
@@ -90,7 +83,7 @@ namespace ZPong
             }
         }
 
-        protected void OnTriggerEnter2D(Collider2D collision)
+        private void OnTriggerEnter2D(Collider2D collision)
         {
             if (collision.gameObject.CompareTag("Paddle"))
             {
@@ -102,20 +95,36 @@ namespace ZPong
 
                 Reflect(newDirection);
                 PlayBounceSound();
+                StartCoroutine(SquishAnimation());
             }
             else if (collision.gameObject.CompareTag("Goal"))
             {
-                // Debug.Log("pos: " + rectTransform.anchoredPosition.x);
-                //Left goal
                 if (this.rectTransform.anchoredPosition.x < -1)
                 {
                     ScoreManager.Instance.ScorePointPlayer2();
                 }
-                //Right goal
                 else
                 {
                     ScoreManager.Instance.ScorePointPlayer1();
                 }
+            }
+        }
+
+        IEnumerator SquishAnimation()
+        {
+            if (!isSquishing)
+            {
+                isSquishing = true;
+
+                Vector3 originalScale = transform.localScale;
+
+                transform.localScale = new Vector3(originalScale.x, originalScale.y * squishScale, originalScale.z);
+                yield return new WaitForSeconds(squishDuration / 2);
+
+                transform.localScale = originalScale;
+                yield return new WaitForSeconds(squishDuration / 2);
+
+                isSquishing = false;
             }
         }
 
@@ -138,7 +147,6 @@ namespace ZPong
         public void SetHeightBounds()
         {
             var height = UIScaler.Instance.GetUIHeightPadded();
-
             screenTop = height / 2;
             screenBottom = -1 * height / 2;
         }
@@ -153,11 +161,10 @@ namespace ZPong
             bounceSFX.pitch = Random.Range(.8f, 1.2f);
             bounceSFX.Play();
         }
-        
+
         public void DisableBall()
         {
             ballActive = false;
         }
-
     }
 }
